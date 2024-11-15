a0f796152d630d54537d8b554d2a56e0ea38a97e
zser99 <wertyyuiop99@naver.com>
2024-11-15 19:01:48 +0900

    0.0.2
    청년대답 로고 표시 이후 3초뒤 로그인 페이지로 자동 이동

--- Front/src/App.js
+++ Front/src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import YouthResponse from './YouthResponse';
 import YouthRecommendation from './YouthRecommendation';
 import CustomPolicySearch from './CustomPolicySearch';
-
+import Login from './Login';
+import Start from './Start';
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<YouthResponse />} />
+
+                <Route path="/" element={<Start />} />
+                <Route path="/Login" element={<Login />} />
+                <Route path="/YouthResponse" element={<YouthResponse />} />
                 <Route path="/recommendation" element={<YouthRecommendation />} />
                 <Route path="/custom-policy-search" element={<CustomPolicySearch />} />
             </Routes>
--- Front/src/YouthResponse.js
+++ Front/src/YouthResponse.js
@@ -38,3 +38,4 @@ function YouthResponse() {
 }
 
 export default YouthResponse;
+
